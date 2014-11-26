e35634c23716cfff0de2b8a2ebf157a0040f9844
Liubko <liubko.qwert@gmail.com>
2014-11-26 14:44:19 +0200

    Stringify

--- main.js
+++ main.js
@@ -27,30 +27,31 @@ $(document).ready(function() {
                 break;
 
             case 'message':
+                $('#app-status-ul').append('<li>--MSG '+JSON.stringify(e.payload) +'--</li>');
                 // if this flag is set, this notification happened while we were in the foreground.
                 // you might want to play a sound to get the user's attention, throw up a dialog, etc.
-                if (e.foreground) {
-                    $('#app-status-ul').append('<li>--INLINE NOTIFICATION--' + '</li>');
+                // if (e.foreground) {
+                //     $('#app-status-ul').append('<li>--INLINE NOTIFICATION--</li>');
 
-                    // on Android soundname is outside the payload.
-                    // On Amazon FireOS all custom attributes are contained within payload
-                    var soundfile = e.soundname || e.payload.sound;
-                    // if the notification contains a soundname, play it.
-                    var my_media = new Media('/android_asset/www/' + soundfile);
-                    my_media.play();
-                } else { // otherwise we were launched because the user touched a notification in the notification tray.
-                    if (e.coldstart) {
-                        $('#app-status-ul').append('<li>--COLDSTART NOTIFICATION--' + '</li>');
-                    } else {
-                        $('#app-status-ul').append('<li>--BACKGROUND NOTIFICATION--' + '</li>');
-                    }
-                }
+                //     // on Android soundname is outside the payload.
+                //     // On Amazon FireOS all custom attributes are contained within payload
+                //     var soundfile = e.soundname || e.payload.sound;
+                //     // if the notification contains a soundname, play it.
+                //     var my_media = new Media('/android_asset/www/' + soundfile);
+                //     my_media.play();
+                // } else { // otherwise we were launched because the user touched a notification in the notification tray.
+                //     if (e.coldstart) {
+                //         $('#app-status-ul').append('<li>--COLDSTART NOTIFICATION--</li>');
+                //     } else {
+                //         $('#app-status-ul').append('<li>--BACKGROUND NOTIFICATION--</li>');
+                //     }
+                // }
 
-                $('#app-status-ul').append('<li>MESSAGE -> MSG: ' + e.payload.message + '</li>');
-                //Only works for GCM
-                $('#app-status-ul').append('<li>MESSAGE -> MSGCNT: ' + e.payload.msgcnt + '</li>');
-                //Only works on Amazon Fire OS
-                $status.append('<li>MESSAGE -> TIME: ' + e.payload.timeStamp + '</li>');
+                // $('#app-status-ul').append('<li>MESSAGE -> MSG: ' + e.payload.message + '</li>');
+                // //Only works for GCM
+                // $('#app-status-ul').append('<li>MESSAGE -> MSGCNT: ' + e.payload.msgcnt + '</li>');
+                // //Only works on Amazon Fire OS
+                // $('#app-status-ul').append('<li>MESSAGE -> TIME: ' + e.payload.timeStamp + '</li>');
                 break;
 
             case 'error':
